35ad29c2c6ebad68caf6de8a6bbce5af6f461cb1
HussainArif <hussainarifkl@gmail.com>
2021-02-01 22:12:11 +0500

    Working build

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+function About() {
+  return (
+    <div>
+      <Head>
+        <title>About page</title>
+      </Head>
+      <p>Hello world from about piece of text</p>
+    </div>
+  );
+}
+export default About;
